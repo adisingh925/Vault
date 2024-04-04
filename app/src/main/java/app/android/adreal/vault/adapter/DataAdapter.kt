0214678c1fd2b8f2443fb0318a86f76ab2f75806
adisingh925 <adisingh925@gmail.com>
2024-04-04 23:04:40 +0530

    dependency upgrades

--- app/src/main/java/app/android/adreal/vault/adapter/DataAdapter.kt
+++ app/src/main/java/app/android/adreal/vault/adapter/DataAdapter.kt
@@ -1,6 +1,7 @@
 package app.android.adreal.vault.adapter
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
