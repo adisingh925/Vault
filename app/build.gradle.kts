363bffdd36b47d0d691ce7e03331f5207f8faced
Aditya Singh <adisingh925@gmail.com>
2024-05-11 15:41:18 +0530

    removed broadcast receiver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,4 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
-
 }
